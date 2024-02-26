d16e7b0e70a69ac6b84524677166ac9bdeeac521
Alexandra <kobatoha@gmail.com>
2024-02-26 23:49:52 +0300

    add animals model

--- DB/Animal.py
+++ DB/Animal.py
@@ -1,9 +1,31 @@
 from DB import Base
-from sqlalchemy import String, Column, Integer, BigInteger
+from sqlalchemy import String, Column, Integer, BigInteger, Date
+from datetime import datetime
 
 
 class Animal(Base):
     __tablename__ = 'animals base'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    id = Column(Integer, primary_key=True)                          # id породы
+    type = Column(String)                                           # вид
+    master = Column(String)                                         # хозяин
+    birth_master = Column(String)                                   # хозяин при рождении
+    name = Column(String)                                           # имя
+    birthday = Column(Date, default=datetime.now().date())          # дата рождения
+    STR = Column(Integer, default=0)                                # сила
+    INT = Column(Integer, default=0)                                # интелект
+    DEX = Column(Integer, default=0)                                # ловкость
+    WIT = Column(Integer, default=0)                                # мудрость
+    CON = Column(Integer, default=0)                                # выносливость
+    MEN = Column(Integer, default=0)                                # духовность
+    mother = Column(String)
+    father = Column(String)
+    children = Column(String)
+
+
+class Fox(Animal):
+    __tablename__ = 'fox type'
+
+
+
+
